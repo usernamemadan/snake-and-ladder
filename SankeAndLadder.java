0f57dbd7b25a16ddc811eff78ac7c6e98ca7fbc2
usernamemadan <69414099+usernamemadan@users.noreply.github.com>
2021-08-23 23:29:31 +0530

    added code to set starting position as zero

--- SankeAndLadder.java
+++ SankeAndLadder.java
@@ -3,7 +3,9 @@ package com.snakeandladder;
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Snake and Ladder Problem");
+		int position=0;
+		System.out.println("Welcome to Snake and Ladder Problem");		
+		System.out.println("starting postion is " + position );
 		
 	}
 
